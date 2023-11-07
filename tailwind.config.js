59d4b7e355db66fda1e8cfc6b2a884807c4f9df6
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-07 21:02:24 +0530

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "neutral-200": "#E4E4E4",
         "neutral-400": "#A2A2A2",
         "neutral-500": "#737373",
+        "neutral-600": "#525252",
         "neutral-700": "#404040",
         "neutral-800": "#262626",
         "neutral-1000": "#000000",
